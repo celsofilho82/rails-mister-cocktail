6f46eb01cba5e8f918e557c142b79a299cab6ff3
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-21 12:16:27 -0300

    add controller action index and view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,6 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def new
@@ -9,5 +10,6 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 end
