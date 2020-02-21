5cd569b745ef6638ecc317b99e4ad8558d2dc148
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-20 23:42:34 -0300

    create new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
-  get 'cocktails/index'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  get 'cocktails/show'
+  resources :cocktails , only: [ :index, :new, :create, :show ] do
+    resources :doses, only: [ :new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
