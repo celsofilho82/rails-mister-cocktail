3395e4d105924954f3e2ea28e62184b0f8e6f048
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-21 12:33:21 -0300

    add cocktail controller and views for new and create

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,12 +4,30 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+        redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
 end
