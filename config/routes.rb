9d26b90ff90757fbb32fbaf184e9895987188261
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-20 23:26:13 -0300

    add controllers, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
