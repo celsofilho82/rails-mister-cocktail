ec72e0112b874a6ece4484044c459916f1ca454f
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-21 18:21:23 -0300

    create, view and remove dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,33 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.ingredient = Ingredient.find(params[:dose][:ingredient])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+# Use callbacks to share common setup or constraints between actions.
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
